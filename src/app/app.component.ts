a188e717a7a163f7cd7e5958d510a09adcd25140
OrgestPasha <orgestpasha2005@gmail.com>
2024-08-19 15:24:23 +0200

    nothing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavigationComponent],
+  imports: [RouterOutlet,CommonModule, NavigationComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'OrgestPortfolio-v2';
+
+
+
+
+  toggleValue:boolean=true;
+
+
+
+  toggleActive={
+    'transform':'translateX(100%)',
+    'transition':'all 200ms ease-out'
+  }
+
 }
